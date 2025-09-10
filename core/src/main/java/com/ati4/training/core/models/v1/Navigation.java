1e76fa004bb562a83c104a35add47515da13b111
Jonas <jonas.fournel@chanel.com>
2025-09-10 16:57:08 +0200

    Adding code

--- core/src/main/java/com/ati4/training/core/models/v1/Navigation.java
+++ core/src/main/java/com/ati4/training/core/models/v1/Navigation.java
@@ -8,7 +8,7 @@ import java.util.List;
 @ConsumerType
 public interface Navigation {
 
-    String RESOURCE_TYPE = "/apps/ati4-training/components/navigation/v1/navigation";
+    String RESOURCE_TYPE = "/apps/ati4-training/components/navigation/v1/header";
 
     List<Page> navigationPages();
 
